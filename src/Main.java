5acf4b2d50197ba59349a5dbd266d408f4057889
joaopsmendes <joao.ps.mendes2011@gmail.com>
2021-03-13 17:08:11 +0000

    criaçao da main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+/**
+ * Football Manager
+ *
+ * @author João Mendes
+ * @author Francisco Paiva
+ * @author Ricardo Silva
+ */
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
