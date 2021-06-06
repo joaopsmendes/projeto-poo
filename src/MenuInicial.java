9b83701948e8cda457f170f4ddfeff02e5e38f21
joaopsmendes <joao.ps.mendes2011@gmail.com>
2021-06-06 17:49:00 +0100

    imprimir o resultado dos jogos

--- src/MenuInicial.java
+++ src/MenuInicial.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -18,11 +19,11 @@ public class MenuInicial {
                         printEquipas(informacoes.getEquipas());
                     }else if(selecao == 3) {
                         printJogadores(informacoes.getJogadores());
-                    }else if(selecao == 4){
+                    }else if(selecao == 4) {
                         System.out.println("Introduza o id do jogador: ");
                         int jogId = scanner.nextInt();
                         Jogador jogador = informacoes.getJogadores().get(jogId);
-                        if(jogador == null){
+                        if (jogador == null) {
                             System.out.println("Id the jogador invalido");
                             continue;
                         }
@@ -31,13 +32,16 @@ public class MenuInicial {
                         String dummy = scanner.nextLine();
                         String nomeEquipa = scanner.nextLine();
                         Equipa equipa = informacoes.getEquipas().get(nomeEquipa);
-                        if(equipa == null){
+                        if (equipa == null) {
                             System.out.println("Nome de equipa invalido");
                             continue;
                         }
 
                         informacoes.transfereJogador(jogId, jogador, equipa.getNome());
                         System.out.println(jogador.getNome() + " foi transferido para " + equipa.getNome());
+                    }else if(selecao==5){
+                        //TODO: verificar
+                        printJogos(informacoes.getJogos());
                     }else if(selecao == 0){
                         quit = true;
                     }else{
@@ -67,6 +71,7 @@ public class MenuInicial {
         System.out.println("        (2) - Ver Equipas");
         System.out.println("        (3) - Ver Jogadores");
         System.out.println("        (4) - Mudar Jogador de Equipa");
+        System.out.println("        (5) - Ver resultados de jogos realizados");
         System.out.println("        (0) - Sair\n");
     }
 
@@ -80,6 +85,12 @@ public class MenuInicial {
         for(Map.Entry<Integer, Jogador> entry : jogadores.entrySet()){
             Jogador jogador = entry.getValue();
             System.out.println("[" + entry.getKey() + "] " + jogador.getNome() + " - " + jogador.getnCamisola() + " - " + jogador.getPosicao() + " - " + jogador.getHistorial().get(jogador.getHistorial().size()-1) + " - " + jogador.calculaOverall());
+        }
+    }
+
+    private void printJogos(List<Jogo> jogos){
+        for(Jogo jogo: jogos){
+            System.out.println("[Jogo] " + jogo.getEquipa1().getNome() + ": " + jogo.getGolosVisitado() + " vs " + jogo.getEquipa2().getNome() + ": " + jogo.getGolosVisitante());
         }
     }
 }
